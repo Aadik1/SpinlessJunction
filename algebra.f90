837150fb7ccba27c2efe4a6d077e2919309f546d
Aadik1 <aadikonidena@gmail.com>
2025-06-10 15:12:03 +0100

    Add files via upload

---
+++ algebra.f90
@@ -0,0 +1,126 @@
+  subroutine BvecA_to_C(A,B,n) !..A is a square matrix, B is a vector, kills original A
+    implicit none 
+    integer :: n,i,k 
+    complex*16, dimension(:,:) :: A(n,n),B(n),C(n,n)
+    complex*16 :: s
+    
+    do i=1,n
+       do k=1,n
+          s=(0.d0,0.d0)
+          s=s+B(k)*A(i,k)
+          C(i,k)=s
+       end do
+    end do
+    A = C
+  end subroutine BvecA_to_C
+  
+  subroutine Hermitian_conjg(A,N,B)
+    implicit none 
+    complex*16 :: A(N,N),B(N,N)
+    integer :: i,j,N
+    
+    do i=1,N
+       do j=1,N
+          B(i,j)=conjg(A(j,i))
+       end do
+    end do
+  end subroutine Hermitian_Conjg
+  
+  subroutine Trace_of_A(A,n,trace) !... complex trace
+    implicit none 
+    integer :: n,i
+    complex*16, dimension(:,:) :: A(n,n)
+    complex*16 :: s, trace
+    
+    s = 0.d0
+    do i=1,n
+       s = s + A(i,i)
+    end do
+    trace=s 
+  end subroutine Trace_of_A
+
+  subroutine vec_cross_product(a,b,c, N)
+    implicit none
+    integer :: N
+    real*8, dimension(N) :: a, b, c
+    
+    c = 0.d0
+    
+    c(1) = a(2)*b(3) - a(3)*b(2)
+    c(2) = a(3)*b(1) - a(1)*b(3)
+    c(3) = a(1)*b(2) - b(2)*a(1)
+  end subroutine vec_cross_product
+
+ real*8  function mod_vec(r)
+    implicit none
+    real*8, dimension(3) :: r
+    real*8 :: modr
+
+    modr = sqrt(r(1)**2 + r(2)**2 + r(3)**2)
+    mod_vec = modr
+  end function mod_vec
+  
+  subroutine AB_to_C(A,B,C,n)
+    implicit none 
+    integer :: n,i,j,k
+    complex*16, dimension(:,:) :: A(n,n),B(n,n),C(n,n)
+    complex*16 :: s
+    
+    do i=1,n
+       do j=1,n
+          s=(0.d0,0.d0)
+          do k=1,n
+             s=s+A(i,k)*B(k,j)
+          end do
+          C(i,j)=s
+       end do
+    end do
+  end subroutine AB_to_C
+  
+  subroutine Trace_of_AB(A,B,n,trace)
+    implicit none 
+    integer :: n,i,j
+    complex*16, dimension(:,:) :: A(n,n), B(n,n)
+    complex*16 :: s
+    real*8 :: trace
+    
+    s = 0.d0
+    do i=1,n
+       do j=1,n
+          s = s + A(i,j)*B(j,i)
+       end do
+    end do
+    trace=s
+  end subroutine Trace_of_AB
+  
+  subroutine Filelen(N, filename)
+    implicit none
+    integer :: N, iostat
+    character(len=300) :: line
+    character(*), intent(in) :: filename
+    
+    open(unit=11, file=filename, status='old', iostat=iostat)
+    if (iostat /= 0) then
+       write(*,*) 'Error reading file', iostat
+       stop
+    end if
+    N = 0
+    do
+       read(11, '(A)', iostat=iostat) line
+       if (iostat /= 0) exit
+       N = N + 1
+    end do
+    close(11)
+    
+  end subroutine Filelen
+  
+  !....................defines the rotation vector about the z-axis
+  subroutine VecAssign(Ri, wi)
+    implicit none
+    real*8, dimension(:) :: Ri(3), wi(3)
+    
+    wi = 0.d0
+    wi(1) = Ri(2)
+    wi(2) = -Ri(1)
+  end subroutine VecAssign
+  
---
+++ stypesub.f90
@@ -0,0 +1,128 @@
+subroutine input()
+  use GreensFunctions 
+  implicit none
+  character :: ver
+  
+  open(2, file='input.dat', status='old')
+  !...order--> 0 : non-interacting, 1: first order interactions, 2: second order interactions 
+  read(2, *) T, V, mu, Volt_range, Gamma0
+  write(*,*) 'T:', T, 'V:', V, 'mu:', mu, 'Volt_range:', Volt_range, gamma0
+  read(2,*) order
+  read(2,*) Natoms
+  write(*,*) 'Order:', order, 'Natoms:', Natoms
+  read(2,*) dw,up,delta
+  write(*,*) dw,up,delta
+  read(2,*) pullay
+  write(*,*) pullay
+  read(2,'(a)') ver
+  verb=.false. ; if(ver.eq.'Y') verb=.true.
+  close(2)
+  beta = 1.d0/(kb * T)
+  
+end subroutine input
+
+subroutine PrintFunctions()
+  use GreensFunctions
+  implicit none
+  integer :: i, j
+  complex*16 :: diff
+
+  open(12, file='info_Hamiltonian.dat', status='replace')
+  
+  write(12,'(/a)') '... Hamiltonian:'
+  do i = 1, Natoms
+     write(12, '(10(a,2f10.5,a))') (' [',H(i,j),'] ', j=1,Natoms)
+  end do
+
+!.... checking if Hermitian  
+
+  write(12,'(/a)') '... Checking the Hamiltonian is Hermitian:'
+  do i=1,Natoms
+     do j=i,Natoms
+        diff=H(i,j)-conjg(H(j,i))
+        write(12,*) i,j,diff
+     end do
+  end do
+  
+  write(12,'(/a)') '... Eigenvalues:'
+  do i = 1, Natoms
+     write(12, *) i,'.', Ev(i)
+  end do
+  
+  write(12,'(/a)') '... Eigenvectors:'
+  do j = 1, Natoms
+     write(12, '(i3,10(a,2f10.5,a))') j,(' [',Eigenvec(i,j),'] ', i = 1, Natoms)
+  end do
+
+  close(12)
+end subroutine PrintFunctions
+
+
+!......finds the integration window for the Landauer paradigm only
+subroutine int_window(a, b, V0)
+  use GreensFunctions
+  implicit none
+  integer :: l
+  logical*8 :: interval
+  real*8 ::  V0, a1, a, b
+  integer :: dw1
+ 
+  interval = .false.
+  
+  if(V0 .gt. 0) then
+     a = mu - V0 - 0.5d0; b = mu + 0.5d0
+  else
+     a = mu - 0.5d0; b = mu - V0 +0.5d0
+  end if
+  dw1  = (b - a)/delta
+  do l = 1, dw1
+     a1 = a + l*delta
+     if(abs(fermi_dist(a1, 0.d0) - fermi_dist(a1, V0)) .gt. delta .and. .not. interval) then 
+        a = a1
+        !print *, 'a loop  works'
+        interval = .true.
+     else if (abs(fermi_dist(a1, 0.d0) - fermi_dist(a1, V0)) .lt. delta .and. interval) then
+        b = a1
+        !print *, 'b loop works'
+     end if
+  end do
+  !print *, 'Lower limit:', a , 'Upper limit:', b, 'Voltage', V0
+  !print *, 'interval distance', (fermi_dist(a, V0) - fermi_dist(b, V0))/step
+end subroutine int_window
+
+real*8 function trans(iw, Volt) !....square bracket terms of Eq. (2) in CHE
+  use GreensFunctions
+  implicit none
+  integer :: iw
+  real*8 :: Volt, trace1, w
+
+  w = omega(iw)
+  work3 = (0.d0, 0.d0)
+
+  work1 = GFf%L(:,:,iw)
+  work2 = GFf%G(:,:,iw)
+  
+  work3 = matmul((im/hbar)*GammaL, (fermi_dist(w, Volt) - 1.d0)*work1 - fermi_dist(w, Volt)*work2) 
+  call trace_of_A(work3, Natoms, trace1)
+  trans = trace1/(2.d0*pi)
+end function trans
+
+real*8 function Current(Volt)
+  use GreensFunctions
+  implicit none
+  real*8 :: Volt, J_L, trans, trace !, w_in, w_f
+  integer :: iw !, iw_in, iw_f
+  
+!......useful for calculating the non-interacting case, finds the integration window
+  !call int_window(w_in, w_f, Volt) 
+  !iw_in = w_grid(w_in)
+  !iw_f = w_grid(w_f)
+
+  J_L = 0.d0
+  do iw = 1, N_of_w
+     trace = trans(iw, Volt)
+     J_L = J_L + trace
+  end do
+  Current = J_L*(delta/hbar)
+end function Current
+  
