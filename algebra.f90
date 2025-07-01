33a36dd0841509dcef212931ca3d6e36466f794e
Aadik1 <aadikonidena@gmail.com>
2025-07-01 15:36:18 +0100

    fixe vec_cross_prod

--- algebra.f90
+++ algebra.f90
@@ -47,7 +47,7 @@
     
     c(1) = a(2)*b(3) - a(3)*b(2)
     c(2) = a(3)*b(1) - a(1)*b(3)
-    c(3) = a(1)*b(2) - b(2)*a(1)
+    c(3) = a(1)*b(2) - b(1)*a(2)
   end subroutine vec_cross_product
 
  real*8  function mod_vec(r)
